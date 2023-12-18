0af4b89e64d3ea27cb9d7183daaa51194a714871
Lalith Kota <kotalalith@gmail.com>
2023-12-18 15:57:36 +0530

    Removed /auth hardcoding for server_url

--- keycloak-init/keycloak_init.py
+++ keycloak-init/keycloak_init.py
@@ -420,7 +420,7 @@ def main():
     fp = open(input_yaml, 'rt')
     values = yaml.load(fp, Loader=yaml.FullLoader)
 
-    server_url = server_url + '/auth/' # Full url to access api
+    #server_url = server_url + '/auth/' # Full url to access api
 
     try:
         print(server_url)
